1e5343b46653726c2e124f9e9cac65cfe08ebd5b
ArseniyMitrofanov <arseniy.2004@inbox.ru>
2022-09-21 18:57:42 +0300

    app added)

--- Burger/ViewController.swift
+++ Burger/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var blur: UIVisualEffectView!
+    @IBOutlet weak var burger: UIButton!
+    let myView = UIStackView()
+    var leftConstraint: NSLayoutConstraint? = nil
+    var topConstraint: NSLayoutConstraint? = nil
+    var hightConsraint: NSLayoutConstraint? = nil
+    var widthConstraint: NSLayoutConstraint? = nil
+    
+    let hehehe = UILabel()
+    let si = UILabel()
+    let cr = UIImageView(image: UIImage(named: "siu"))
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myView.axis = .vertical
+        hehehe.text = "hehehe..."
+        si.text = "SIUUUUUUUUUUU"
+        myView.addArrangedSubview(hehehe)
+        myView.addArrangedSubview(si)
+        myView.addArrangedSubview(cr)
+        
+        blur.isHidden = true
+        myView.backgroundColor = .lightGray
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(myView)
+        leftConstraint = myView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: -300)
+        topConstraint = myView.topAnchor.constraint(equalTo: self.view.topAnchor)
+        hightConsraint = myView.heightAnchor.constraint(equalTo: self.view.heightAnchor)
+        widthConstraint = myView.widthAnchor.constraint(equalToConstant: 300)
+        NSLayoutConstraint.activate([
+            leftConstraint!, topConstraint!, hightConsraint!, widthConstraint!
+        ])
     }
-
-
+    
+    
+    @IBAction func burgerTapped(_ sender: Any) {
+        blur.isHidden = false
+        self.view.bringSubviewToFront(myView)
+        if self.leftConstraint != nil {
+            UIView.animate(withDuration: 0.5){
+                self.leftConstraint!.constant = 0
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
 }
 
